329d7538cb49e1ea141b9dd2b65e054602d53052
Aboobaker Siddique <siddiqueaboobaker905@gmail.com>
2026-07-09 15:10:17 +0530

    login simulation solved

--- week 1/Day 4/login_simulation.py
+++ week 1/Day 4/login_simulation.py
@@ -1,22 +1,35 @@
-print("            Login Simulation       ")
+print("         Login Simulation         ")
 print("-------------------------------------")
-username="admin"
-password="password@123"
+
+username = "admin"
+password = "password@123"
 attempts = 3
+
+
 def login():
     user_username = input("Enter your username: ")
     user_password = input("Enter your password: ")
+
     if user_username == username and user_password == password:
         print("✅ Login successful!")
+        return True
+    
+    elif user_username == username:
+        print("❌ Invalid password.")
+        return False
+    
     else:
-        print("❌ Invalid username or password. Please try again.")
-        attempts -= 1
-        print(f"Remaining attempts: {attempts}")
-        
-        
+        print("❌ Invalid username or password.")
+        return False
+
 
 while attempts > 0:
-    login()
+
+    if login():
+        break
+
     attempts -= 1
+    print(f"Remaining attempts: {attempts}")
+
     if attempts == 0:
-    print("Account Locked !!!!!!!")
+        print("🚫 Account Locked!!!!!!")
